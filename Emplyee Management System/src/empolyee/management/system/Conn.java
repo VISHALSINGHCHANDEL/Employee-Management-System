dc3963899f375f07b394769b376aa68fb0b8bf46
VISHAL RAJ <102964349+VISHALSINGHCHANDEL@users.noreply.github.com>
2023-06-21 09:30:37 +0530

    Add files via upload

---
+++ Emplyee Management System/src/empolyee/management/system/Conn.java
@@ -0,0 +1,22 @@
+
+package empolyee.management.system;
+ import java.sql.*;
+
+public class Conn {
+    Connection c;
+    Statement s;
+    
+    
+    public Conn() {
+      try {
+       Class.forName("com.mysql.cj.jdbc.Driver");
+       c= DriverManager.getConnection("jdbc:mysql:///employeemanagementsystem","root","vishal");
+       s= c.createStatement();
+       
+      } catch (Exception e) {
+          e.printStackTrace();
+          
+          
+      }
+    }  
+}
